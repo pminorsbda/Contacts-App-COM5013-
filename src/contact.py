c0e69b73ac3d6c482a38a2be068dda5757608b4a
lilrealdeal.candy@gmail.com <lilrealdeal.candy@gmail.com>
2025-12-04 09:39:14 +0000

    Update contact.py

--- src/contact.py
+++ src/contact.py
@@ -1,1 +1,9 @@
+class Contact:
+    def __init__(self, name: str, phone: str, email: str, address: str = ""):
+        self.name = name
+        self.phone = phone
+        self.email = email
+        self.address = address
 
+    def __repr__(self) -> str:
+        return f"Contact(name={self.name!r}, phone={self.phone!r})"
